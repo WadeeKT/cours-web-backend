e81362f71562389c68f234a94320d87f1d028fed
WadeeKT <thomassanna1e@gmail.com>
2024-01-18 14:40:11 +0100

    Init

---
+++ exercice-1CH1.2.1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonjour</title>
+</head>
+    <?php
+    $date = Date("j/m/Y");
+    $heure = Date("H");
+    echo "<h3> Nous sommes le $date et il est $heure heures. </h3>"
+    ?>
+    <?php
+    if ($heure <
+    echo ""
+    ?>
+</body>
+</html>
