2ee70dff81b8ab4c3a251d82118626f849d50f9b
WadeeKT <thomassanna1e@gmail.com>
2024-01-18 14:56:53 +0100

    Exercice CH1.2.2 - Les fonctions

---
+++ exercice-CH1.2.2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Est Bissextile - Fonction</title>
+</head>
+<body>
+    <?php
+    function estBissextile($a){
+        if (($a%4==0 && $a%100!=0) || $a%400==0){
+            return $a;
+        }
+    }
+
+    echo "<h3> Liste des années bissectiles entre 2020 et 2050 : </h3>";
+
+    for($i=2020 ; $i <= 2050 ; $i++){
+        echo estBissextile($i);
+        echo " ";
+    }
+    ?>
+</body>
+</html>
