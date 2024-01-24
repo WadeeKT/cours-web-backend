b08549c607bcb0845ebcc06e25f33711cfc52f69
WadeeKT <thomassanna1e@gmail.com>
2024-01-24 13:34:41 +0100

    update

---
+++ CH1.3-formulaires/exercice-CH1.3/action.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $tabEtu = [
+        "Paoli" => 10,
+        "Nivet" => 19,
+        "Simonnet" => 18
+    ];
+
+    if(isset($_POST['recherche-submit'])){
+        $nomEtu = $_POST['nomEtu'];
+        if (isset($tabEtu[$nomEtu])){
+            $note = $tabEtu[$nomEtu];
+            echo"$nomEtu a une note de $note";
+        } else {
+            echo "Il n'existe pas de $nomEtu dans la table";
+        }
+        
+    }
+
+    ?>
+</body>
+</html>
